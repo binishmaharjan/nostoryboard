f2d18b8933fa3d4d25622214bf2979f643e96e9c
Binish Maharjan <guest@781cm030.local>
2018-06-01 11:09:46 +0900

    Initial Commit

---
+++ NoStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoStoryboard
+//
+//  Created by guest on 2018/06/01.
+//  Copyright © 2018年 guest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
